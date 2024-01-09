bc4f5dcc83f9e3fbf437c0d9dd95ebebb09e5f21
LuongMinhNhatSE160865 <75418809+Misagivn@users.noreply.github.com>
2024-01-09 16:49:20 +0700

    Demo home screen, remake navbar - not complete
    Thay đổi navbar, demo feature content card trên main screen (chưa hoàn thành

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -1,51 +1,102 @@
 import { Link } from "@nextui-org/link";
 import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code"
+import { Code } from "@nextui-org/code";
 import { button as buttonStyles } from "@nextui-org/theme";
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
+import React from "react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Divider,
+  Image,
+} from "@nextui-org/react";
 
 export default function Home() {
-	return (
-		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-			<div className="inline-block max-w-lg text-center justify-center">
-				<h1 className={title()}>Make&nbsp;</h1>
-				<h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
-				<br />
-				<h1 className={title()}>
-					websites regardless of your design experience.
-				</h1>
-				<h2 className={subtitle({ class: "mt-4" })}>
-					Beautiful, fast and modern React UI library.
-				</h2>
-			</div>
-
-			<div className="flex gap-3">
-				<Link
-					isExternal
-					href={siteConfig.links.docs}
-					className={buttonStyles({ color: "primary", radius: "full", variant: "shadow" })}
-				>
-					Documentation
-				</Link>
-				<Link
-					isExternal
-					className={buttonStyles({ variant: "bordered", radius: "full" })}
-					href={siteConfig.links.github}
-				>
-					<GithubIcon size={20} />
-					GitHub
-				</Link>
-			</div>
-
-			<div className="mt-8">
-				<Snippet hideSymbol hideCopyButton variant="flat">
-					<span>
-						Get started by editing <Code color="primary">app/page.tsx</Code>
-					</span>
-				</Snippet>
-			</div>
-		</section>
-	);
+  //This is Just Demo data before having API just to check the UI
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <Card className="max-w-[1250px]">
+        <CardHeader className="flex gap-3 ">
+          <div className="flex flex-col">
+            <div className="text-4xl text-cyan-500">For Sale</div>
+            <div className="text-6xl">Feature Products</div>
+          </div>
+        </CardHeader>
+        <CardBody>
+          <div className="grid grid-cols-4 grid-rows-4 gap-5 text-xl max-w-[1250px]">
+            <Card className="w-[250px] h-[250px] bg-gradient-to-r from-cyan-500 to-blue-500">
+              <CardHeader>
+                <h1 className="text-lg text-amber-200">Siu chung cu cua PhucNVM</h1>
+              </CardHeader>
+              <CardBody>
+                <Image
+                  removeWrapper
+                  alt="hinh1"
+                  width="100%"
+                  height="100%"
+                  className="z-0 w-full h-full object-cover"
+                  src="./demo.png"
+                />
+                <CardFooter className="justify-center">
+                  <p className="text-sm">10ty/can</p>
+                </CardFooter>
+              </CardBody>
+            </Card>
+            <div className="w-[100] h-[100] bg-gradient-to-r from-cyan-500 to-blue-500">
+              This is testing
+            </div>
+            <div className="w-[100] h-[100] bg-gradient-to-r from-cyan-500 to-blue-500">
+              This is testing
+            </div>
+            <div className="w-[100] h-[100] bg-gradient-to-r from-cyan-500 to-blue-500">
+              This is testing
+            </div>
+            <div className="w-[100] h-[100] bg-gradient-to-r from-cyan-500 to-blue-500">
+              This is testing
+            </div>
+            <div className="w-[100] h-[100] bg-gradient-to-r from-cyan-500 to-blue-500">
+              This is testing
+            </div>
+            <div className="w-[100] h-[100] bg-gradient-to-r from-cyan-500 to-blue-500">
+              This is testing
+            </div>
+            <div className="w-[100] h-[100] bg-gradient-to-r from-cyan-500 to-blue-500">
+              This is testing
+            </div>
+            <div className="w-[100] h-[100] bg-gradient-to-r from-cyan-500 to-blue-500">
+              This is testing
+            </div>
+            <div className="w-[100] h-[100] bg-gradient-to-r from-cyan-500 to-blue-500">
+              This is testing
+            </div>
+            <div className="w-[100] h-[100] bg-gradient-to-r from-cyan-500 to-blue-500">
+              This is testing
+            </div>
+            <div className="w-[100] h-[100] bg-gradient-to-r from-cyan-500 to-blue-500">
+              This is testing
+            </div>
+            <div className="w-[100] h-[100] bg-gradient-to-r from-cyan-500 to-blue-500">
+              This is testing
+            </div>
+            <div className="w-[100] h-[100] bg-gradient-to-r from-cyan-500 to-blue-500">
+              This is testing
+            </div>
+            <div className="w-[100] h-[100] bg-gradient-to-r from-cyan-500 to-blue-500">
+              This is testing
+            </div>
+            <div className="w-[100] h-[100] bg-gradient-to-r from-cyan-500 to-blue-500">
+              This is testing
+            </div>
+          </div>
+        </CardBody>
+        <CardFooter>
+          <p>Day la footer coi nhu the nao</p>
+        </CardFooter>
+      </Card>
+    </section>
+  );
 }
--- front-end/components/navbar.tsx
+++ front-end/components/navbar.tsx
@@ -57,7 +57,7 @@ export const Navbar = () => {
 				<NavbarBrand as="li" className="gap-3 max-w-fit">
 					<NextLink className="flex justify-start items-center gap-1" href="/">
 						<Logo />
-						<p className="font-bold text-inherit">ACME</p>
+						<p className="font-bold text-inherit">Dream Share</p>
 					</NextLink>
 				</NavbarBrand>
 				<ul className="hidden lg:flex gap-4 justify-start ml-2">
@@ -83,15 +83,6 @@ export const Navbar = () => {
 				justify="end"
 			>
 				<NavbarItem className="hidden sm:flex gap-2">
-					<Link isExternal href={siteConfig.links.twitter} aria-label="Twitter">
-						<TwitterIcon className="text-default-500" />
-					</Link>
-					<Link isExternal href={siteConfig.links.discord} aria-label="Discord">
-						<DiscordIcon className="text-default-500" />
-					</Link>
-					<Link isExternal href={siteConfig.links.github} aria-label="Github">
-						<GithubIcon className="text-default-500" />
-					</Link>
 					<ThemeSwitch />
 				</NavbarItem>
 				<NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
