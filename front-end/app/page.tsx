ee83d6b97c4bcb00f7d076f34b221936c91658b8
LuongMinhNhatSE160865 <75418809+Misagivn@users.noreply.github.com>
2024-01-13 12:59:11 +0700

    Optimize the directory, remove redundant files and incorrect imports, and establish a back-end folder.
    Optimize the directory, remove redundant files and incorrect imports, and establish a back-end folder.

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -1,102 +1,74 @@
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code";
-import { button as buttonStyles } from "@nextui-org/theme";
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
+"use client";
 import React from "react";
-import {
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-  Divider,
-  Image,
-} from "@nextui-org/react";
+
+import axios, { Axios } from "axios";
+import { Input } from "@nextui-org/react";
+import { Button } from "@nextui-org/react";
+
+const baseURL = "https://6548b3e4dd8ebcd4ab236ce6.mockapi.io/Nhanvien";
 
 export default function Home() {
   //This is Just Demo data before having API just to check the UI
+
+  //tạo các biến để sử dụng co các hàm dưới
+  const [post, setPost] = React.useState(null);
+  const [emailValue, setValue] = React.useState("myemail@email.com");
+
+  //Hàm này để check format của Email có đúng hay là không
+  const validateEmail = (emailValue: string) =>
+    emailValue.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i);
+  const phonePattern = /^0[0-9]{7,9}$/;
+
+  const isInvalid = React.useMemo(() => {
+    if (emailValue === "") return false;
+
+    return validateEmail(emailValue) ? false : true;
+  }, [emailValue]);
+
+  //Demo - thử làm hàm tạo nhân viên API
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPost(response.data);
+    });
+  });
+
+  function createNhanVien() {
+    axios
+      .post(`${baseURL}/1`, {
+        emailValue,
+        name: "default",
+        phon: "testAPI",
+      })
+      .then((response) => {
+        console.log(response);
+        // Handle response
+      });
+  }
+
+  if (!post) return "No post!";
+
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <Card className="max-w-[1250px]">
-        <CardHeader className="flex gap-3 ">
-          <div className="flex flex-col">
-            <div className="text-4xl text-cyan-500">For Sale</div>
-            <div className="text-6xl">Feature Products</div>
-          </div>
-        </CardHeader>
-        <CardBody>
-          <div className="grid grid-cols-4 grid-rows-4 gap-5 text-xl max-w-[1250px]">
-            <Card className="w-[250px] h-[250px] bg-gradient-to-r from-cyan-500 to-blue-500">
-              <CardHeader>
-                <h1 className="text-lg text-amber-200">Siu chung cu cua PhucNVM</h1>
-              </CardHeader>
-              <CardBody>
-                <Image
-                  removeWrapper
-                  alt="hinh1"
-                  width="100%"
-                  height="100%"
-                  className="z-0 w-full h-full object-cover"
-                  src="./demo.png"
-                />
-                <CardFooter className="justify-center">
-                  <p className="text-sm">10ty/can</p>
-                </CardFooter>
-              </CardBody>
-            </Card>
-            <div className="w-[100] h-[100] bg-gradient-to-r from-cyan-500 to-blue-500">
-              This is testing
-            </div>
-            <div className="w-[100] h-[100] bg-gradient-to-r from-cyan-500 to-blue-500">
-              This is testing
-            </div>
-            <div className="w-[100] h-[100] bg-gradient-to-r from-cyan-500 to-blue-500">
-              This is testing
-            </div>
-            <div className="w-[100] h-[100] bg-gradient-to-r from-cyan-500 to-blue-500">
-              This is testing
-            </div>
-            <div className="w-[100] h-[100] bg-gradient-to-r from-cyan-500 to-blue-500">
-              This is testing
-            </div>
-            <div className="w-[100] h-[100] bg-gradient-to-r from-cyan-500 to-blue-500">
-              This is testing
-            </div>
-            <div className="w-[100] h-[100] bg-gradient-to-r from-cyan-500 to-blue-500">
-              This is testing
-            </div>
-            <div className="w-[100] h-[100] bg-gradient-to-r from-cyan-500 to-blue-500">
-              This is testing
-            </div>
-            <div className="w-[100] h-[100] bg-gradient-to-r from-cyan-500 to-blue-500">
-              This is testing
-            </div>
-            <div className="w-[100] h-[100] bg-gradient-to-r from-cyan-500 to-blue-500">
-              This is testing
-            </div>
-            <div className="w-[100] h-[100] bg-gradient-to-r from-cyan-500 to-blue-500">
-              This is testing
-            </div>
-            <div className="w-[100] h-[100] bg-gradient-to-r from-cyan-500 to-blue-500">
-              This is testing
-            </div>
-            <div className="w-[100] h-[100] bg-gradient-to-r from-cyan-500 to-blue-500">
-              This is testing
-            </div>
-            <div className="w-[100] h-[100] bg-gradient-to-r from-cyan-500 to-blue-500">
-              This is testing
-            </div>
-            <div className="w-[100] h-[100] bg-gradient-to-r from-cyan-500 to-blue-500">
-              This is testing
-            </div>
-          </div>
-        </CardBody>
-        <CardFooter>
-          <p>Day la footer coi nhu the nao</p>
-        </CardFooter>
-      </Card>
+      <h1 className="text-3xl">Test demo API:</h1>
+      <p>{post.gender}</p>
+      <p>{post.email}</p>
+      <form action="" id="login" method="post" onSubmit={createNhanVien}>
+        <Input
+          value={emailValue}
+          type="email"
+          label="Email"
+          variant="bordered"
+          isInvalid={isInvalid}
+          color={isInvalid ? "danger" : "success"}
+          errorMessage={isInvalid && "Please enter a valid email"}
+          onValueChange={setValue}
+          className="max-w-xs"
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <Button color="primary" type="submit">
+          Submit
+        </Button>
+      </form>
     </section>
   );
 }
