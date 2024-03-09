b256262aab41c2bdb0c30a3dc9055a0ece5377a2
Anh003 <146794820+Subinn03@users.noreply.github.com>
2024-03-09 07:22:06 +0700

    Add Staff Manager

---
+++ front-end/app/console/staffManager/data.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export function getStaff() {
+  const axios = require("axios");
+  const [staff, setStaff] = useState(null); //Tạo state Staff
+
+  const baseURL = "http://localhost:3000";
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/staffs/", {})
+      .then(function (res) {
+        console.log(res.data.staffs);
+        setProduct(res.data.staffs);
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+    return () => setStaff(null);
+  }, []);
+  return staff;
+}
+
+const columns = [
+  { name: "ID", uid: "id", sortable: true },
+  { name: "NAME", uid: "name", sortable: true },
+  { name: "DESCRIPTION", uid: "description", sortable: false },
+  { name: "STATUS", uid: "status", sortable: true },
+];
+
+const statusOptions = [
+  { name: "Active", uid: "active" },
+  { name: "Pause", uid: "paused" },
+  { name: "Cancel", uid: "cancel" },
+];
+
+export { columns, statusOptions };
